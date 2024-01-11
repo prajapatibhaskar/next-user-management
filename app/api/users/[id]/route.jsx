9fc0887fd0a3b5bd040709c17fa03fb83cfcc76f
Bhaskar Prajapati <prajapatibhaskarbp@gmail.com>
2024-01-11 23:33:58 +0530

    Implement get specific user data api

--- app/api/users/[id]/route.jsx
+++ app/api/users/[id]/route.jsx
@@ -1,2 +1,11 @@
 import { users } from "@/app/util/db";
 import { NextResponse } from "next/server";
+
+// 2. Get specific user by id
+export async function GET(req, res) {
+  const { id } = await res.params;
+
+  const user = users.filter((user) => user.id === id);
+
+  return NextResponse.json({ user });
+}
