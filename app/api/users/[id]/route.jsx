aa739a518bff74b829cc0bba843ff6839749e07c
Bhaskar Prajapati <prajapatibhaskarbp@gmail.com>
2024-01-12 00:04:14 +0530

    Implement user login api

--- app/api/users/[id]/route.jsx
+++ app/api/users/[id]/route.jsx
@@ -9,3 +9,19 @@ export async function GET(req, res) {
 
   return NextResponse.json({ user });
 }
+
+// 3. Login
+export async function POST(req, res) {
+  const { name, email, password } = await req.json();
+  const { id } = await res.params;
+
+  const { name: uName, email: uEmail, password: uPassword } = users.find((user) => user.id === id);
+
+  if (uName === name && uEmail === email && uPassword === password) {
+    return NextResponse.json({ result: "Successfully Logged In" });
+  } else if (!name || !email || !password) {
+    return NextResponse.json({ result: "Missing required Fields" });
+  } else {
+    return NextResponse.json({ result: "Invalid Credentials" });
+  }
+}
